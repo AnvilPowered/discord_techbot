3ff7ad2134451cf476c017f27b8b842ffbb17a3f
Pablo González <pagoru@gmail.com>
2017-07-26 16:35:21 +0200

    Minor text fixes

--- main.js
+++ main.js
@@ -8,3 +8,15 @@ try{
 }
 
 require('./bot/bot').init();
+
+// const fs = require('fs');
+// const path = require('path');
+// const Discord = require('discord.js');
+//
+// global.bot = new Discord.Client();
+//
+// bot.login(global.config.bot.token).then(() => {
+//     bot.channels.get('282042352435200001').fetchMessage('339115406885257217').then(message => {
+//         message.edit(fs.readFileSync(path.join(__dirname, '/messages/server-updates')).toString());
+//     }).catch(console.error);
+// });
